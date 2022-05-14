999a5165e2f4276a006053a052b0daa498be46b4
foolishmax <827864735@qq.com>
2022-05-15 01:04:19 +0800

    feat(package): 新增models/package包

--- core/exec/lib/index.js
+++ core/exec/lib/index.js
@@ -1,7 +1,11 @@
 'use strict';
 
+const Package = require('@lbs-cli-dev/package');
+
 module.exports = exec;
 
 function exec() {
+  const pkg = new Package();
+  console.log(pkg);
   console.log(process.env.CLI_TARGET_PATH);
 }
---
+++ models/package/README.md
@@ -0,0 +1,11 @@
+# `@lbs-cli-dev/package`
+
+> TODO: description
+
+## Usage
+
+```
+const package = require('@lbs-cli-dev/package');
+
+// TODO: DEMONSTRATE API
+```
