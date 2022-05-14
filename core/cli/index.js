47db24caa4430729f129d020bc0e10696edc2662
foolishmax <827864735@qq.com>
2022-05-14 17:09:36 +0800

    feat(command registry): 脚手架命令注册

--- core/cli/index.js
+++ core/cli/index.js
@@ -8,12 +8,13 @@ const colors = require('colors/safe');
 const log = require('@lbs-cli-dev/log');
 const pathExists = require('path-exists').sync;
 const userHome = require('user-home');
-const dedent = require('dedent');
+const commander = require('commander');
 
 const pkg = require('./package.json');
 const constant = require('./lib/const');
 
 let args;
+const program = new commander.Command();
 
 async function core() {
   try {
@@ -21,12 +22,42 @@ async function core() {
     checkNodeVersion();
     checkRoot();
     checkUserHome();
-    checkInputArgs();
+    // checkInputArgs();
     checkEnv();
     checkGlobalUpdate();
+    registerCommand();
   } catch(e) {
     log.error(e.message);
   }
+}
+
+function registerCommand() {
+  program
+    .name(Object.keys(pkg.bin)[0])
+    .usage('<command> [options]')
+    .version(pkg.version)
+    .option('-d, --debug', '是否开启调试模式', false)
+
+
+  // 开启debug模式
+  program.on('option:debug', function() {
+    process.env.LOG_LEVEL = 'verbose';
+    log.level = process.env.LOG_LEVEL;
+  })
+
+  // 未知命令处理
+  program.on('command:*', function(obj) {
+    const availableCommands =  program.commands.map(cmd => cmd.name());
+    console.log(colors.red('未知的命令：'+obj[0]));
+    availableCommands.length && console.log(colors.red('可用命令：' + availableCommands.join(',')));
+  })
+
+  if (program.args?.length < 1) {
+    program.outputHelp();
+    console.log();
+  }
+
+  program.parse(process.argv);
 }
 
 async function checkGlobalUpdate() {
